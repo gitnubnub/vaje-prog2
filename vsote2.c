3504fc975be80406a209023dfb16ac027a3f588c
Urška <137105614+gitnubnub@users.noreply.github.com>
2023-06-21 16:07:44 +0200

    Update vsote2.c
    formatirala po svoje

--- vsote2.c
+++ vsote2.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int nacini(int n,int k, int* p, int j){
-    if(n < k) k = n;
+int nacini(int n, int k, int* p, int j) {
+    if (n < k) {
+        k = n;
+    }
 
-    if(k == 0){
-        if(n > 0) return 0;
+    if (k == 0) {
+        if (n > 0) {
+            return 0;
+        }
 
-        for(int i = 0; i < j; i++){
-            if(i > 0) printf(" + ");
+        for (int i = 0; i < j; i++) {
+            if (i > 0) {
+                printf(" + ");
+            }
             printf("%d", p[i]);
         }
         printf("\n");
@@ -16,21 +22,21 @@ int nacini(int n,int k, int* p, int j){
     }
 
     p[j] = k;
-    nacini(n-k,k,p,j+1);
+    nacini(n - k, k, p, j + 1);
     p[j] = 0;
-    nacini(n, k-1,p,j);
+    nacini(n, k - 1, p, j);
 
     return 0;
 }
 
-int main(){
+int main() {
     int n = 0;
     int k = 0;
 
     scanf("%d%d", &n, &k);
 
-    int* p = calloc(n,sizeof(int));
+    int* p = (int*) calloc(n, sizeof(int));
 
-    nacini(n,k,p,0);
+    nacini(n, k, p, 0);
     return 0;
 }
